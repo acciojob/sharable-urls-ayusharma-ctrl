29cf4e21fbb5fbb5f117b6ae39ae3ec2f78e5130
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-14 13:32:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+var btn = document.getElementById("button");
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    var url;
+    if (name != "" && year != "") {
+        url = "https://localhost:8080/?" + "name=" + name + "&year=" + year;
+    } else if (name != "") {
+        url = "https://localhost:8080/?" + "name=" + name;
+    } else if (year != "") {
+        url = "https://localhost:8080/?" + "year=" + year;
+    } else {
+        url = "https://localhost:8080/";
+    }
+    document.getElementById("url").innerHTML = url;
+});
